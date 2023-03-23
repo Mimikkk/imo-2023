80d8d493ec82057f95c7f55a15ed25854ad488d1
Daniel Zdancewicz <d.zdancewicz@teb-akademia.pl>
2023-03-23 18:52:09 +0100

    finally cyclic?

---
+++ Domain/Structures/Moves.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Extensions;
+
+namespace Domain.Structures;
+
+public static class Moves {
+  public static void Insert(IList<Node> cycle, Node node, (Node a, Node b) edge) {
+    if (edge.a == cycle.First() && edge.b == cycle.Last() || edge.b == cycle.First() && edge.a == cycle.Last()) {
+      cycle.Add(node);
+      return;
+    }
+
+
+    var ia = cycle.IndexOf(edge.a);
+    var ib = cycle.IndexOf(edge.b);
+    cycle.Insert(ia > ib ? ia : ib, node);
+  }
+
+  public static void ExchangeVertex(IList<Node> first, IList<Node> second, Node a, Node b) {
+    var (ia, ib) = (first.IndexOf(a), second.IndexOf(b));
+    first.Remove(a);
+    first.Insert(ia, b);
+    second.Remove(b);
+    second.Insert(ib, a);
+  }
+
+  public static void ExchangeVertex(IList<Node> cycle, Node a, Node b) => cycle.Swap(a, b);
+
+  public static void ExchangeEdge(IList<Node> cycle, Node a, Node b) {
+    var ia = cycle.IndexOf(a);
+    var ib = cycle.IndexOf(b);
+    if (ia > ib) (ia, ib) = (ib, ia);
+    if (ia == 0 && ib == cycle.Count - 1) cycle.Swap(ia, ib);
+
+    var elementCount = ib - ia + 1;
+    for (var i = 0; i < elementCount / 2; ++i) {
+      cycle.Swap((ia + cycle.Count + i) % cycle.Count, (ib + cycle.Count - i) % cycle.Count);
+    }
+  }
+}
--- Interface/Modules/CyclePanelModule.cs
+++ Interface/Modules/CyclePanelModule.cs
@@ -12,46 +12,6 @@ using static Domain.Extensions.EnumerableExtensions;
 namespace Interface.Modules;
 
 internal sealed record CyclePanelModule {
-  private static class Moves {
-    public static void Insert(IList<Node> cycle, Node node, (Node a, Node b) edge) {
-      if (edge.a == cycle.First() && edge.b == cycle.Last() || edge.b == cycle.First() && edge.a == cycle.Last()) {
-        cycle.Add(node);
-        return;
-      }
-
-
-      var ia = cycle.IndexOf(edge.a);
-      var ib = cycle.IndexOf(edge.b);
-      cycle.Insert(ia > ib ? ia : ib, node);
-    }
-
-    public static void ExchangeVertex(IList<Node> first, IList<Node> second, Node a, Node b) {
-      var (ia, ib) = (first.IndexOf(a), second.IndexOf(b));
-      first.Remove(a);
-      first.Insert(ia, b);
-      second.Remove(b);
-      second.Insert(ib, a);
-    }
-
-    public static void ExchangeVertex(IList<Node> cycle, Node a, Node b) => cycle.Swap(a, b);
-
-    public static void ExchangeEdge(IList<Node> cycle, Node a, Node b) {
-      var ia = cycle.IndexOf(a);
-      var ib = cycle.IndexOf(b);
-
-      if (ia > ib) (ia, ib) = (ib, ia);
-      if (ia == 0 && ib == cycle.Count - 1) cycle.Swap(ia, ib);
-      if (ia == ib) return;
-
-
-      while (ia < ib) {
-        (cycle[ia], cycle[ib]) = (cycle[ib], cycle[ia]);
-        ++ia;
-        --ib;
-      }
-    }
-  }
-
   public CyclePanelModule(MainWindow window) {
     Self = window;
 
@@ -152,5 +112,5 @@ internal sealed record CyclePanelModule {
   private MemoryModule M => Self.Mod.Memory;
   private MouseModule Mouse => Self.Mod.Mouse;
   public void Subscribe(Action update) => Updates.Add(update);
-  public List<List<Node>> Cycles = new();
+  public readonly List<List<Node>> Cycles = new();
 }
